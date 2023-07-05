69f161a1f0d2b61bdb5e7ad90e62ce8003837377
seki.junichi <seki.junichi@PM12-003.local>
2023-07-05 13:59:05 +0900

    Initial setup and Terraform verification

---
+++ dev/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  profile = "terraform"
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "hello-world" {
+  ami = "ami-05ffd9ad4ddd0d6e2"
+  instance_type = "t2.micro"
+}
