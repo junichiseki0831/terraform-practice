bb5204f153d9c778de5a527f7ca74cab97d1d3f4
seki.junichi <seki.junichi@PM12-003.local>
2023-07-16 16:33:00 +0900

    Create NAT gateway and Elastic IP

--- dev/main.tf
+++ dev/main.tf
@@ -43,4 +43,8 @@ module "network" {
   private_subnet_name_1a   = "${local.project}-${local.env}-private-subnet-${local.availability_zone_1a}"
   private_subnet_name_1c   = "${local.project}-${local.env}-private-subnet-${local.availability_zone_1c}"
   internet_gateway_name    = "${local.project}-${local.env}-igw"
+  elastic_ip_name_1a       = "${local.project}-${local.env}-eip-${local.availability_zone_1a}"
+  elastic_ip_name_1c       = "${local.project}-${local.env}-eip-${local.availability_zone_1c}"
+  nat_gateway_name_1a      = "${local.project}-${local.env}-ngw-${local.availability_zone_1a}"
+  nat_gateway_name_1c      = "${local.project}-${local.env}-ngw-${local.availability_zone_1c}"
 }
